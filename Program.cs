c871eb6fb8e71fe5235bacd113241b2e3c03a357
Fanis-Galimyanov <fanisgalimyanov@gmail.com>
2022-02-15 13:11:12 +0300

    Добавил WiteLoine

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace GitTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello IT Park!!!!");
+            Console.WriteLine("Hello IT Park!!!!");
         }
     }
 }
